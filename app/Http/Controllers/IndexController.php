0ef7b456682ee9a924a41e7010bb7af15b688b6d
evansmwenda <evansmwenda.em@gmail.com>
2020-06-04 23:42:50 +0300

    random ordered products

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -10,7 +10,13 @@ class IndexController extends Controller
 {
     //
     public function index(){
-    	$products = Product::orderBy('id','DESC')->get();
+    	//in descending order
+    	// $products = Product::orderBy('id','DESC')->get();
+
+    	//in random order
+    	$products = Product::inRandomOrder()->get();
+
+
     	$main_categories = Category::where('parent_id',0)->get();
     	// dd($main_categories);
     	return view('index')->with(compact('products','main_categories'));
