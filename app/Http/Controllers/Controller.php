0e93956e64a48ee40b7e5c66c30faaf2b2296a42
evansmwenda <evansmwenda.em@gmail.com>
2020-06-10 12:09:05 +0300

    updated header menu to include our categories

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,16 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+use App\Category;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public static function mainCategories(){
+    	$mainCategories = Category::where(['parent_id' => 0])->get();
+    	// $mainCategories= json_decode(json_encode($mainCategories));
+    	// echo "<pre>";print_r($mainCategories);
+    	return $mainCategories;
+    } 
 }
